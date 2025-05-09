0abf857a85120fd13f09188b4ac9336ab07640fd
Jasminee95 <jasmine_745@hotmail.com>
2025-05-09 10:50:53 +0200

    9.05.25

---
+++ Gjett Tallet/Program.cs
@@ -0,0 +1,55 @@
+﻿ 
+     Random random = new Random();
+     int randomNumber = random.Next(1, 101); 
+     Console.WriteLine("Jeg tenker på et tall mellom 1 og 100, hva gjetter du?");
+     
+
+     string input;
+     
+     while (true)
+     {
+         input = Console.ReadLine();
+         
+         if (int.TryParse(input, out int userGuess))
+         {
+             if (userGuess == randomNumber)
+             {
+                 Console.WriteLine("Riktig, bra gjetta!! Vil du kjøre en runde til?");
+                 string svar = Console.ReadLine();
+                 if (svar.ToLower() == "ja")
+                 {
+                     randomNumber = random.Next(1, 101);
+                     Console.WriteLine("Jeg tenker på et tall mellom 1 og 100, hva gjetter du?");
+                 }
+                 else
+                 {
+                     Console.WriteLine("Takk for nå! Hadeee!");
+                     break;
+                 }
+             }
+             else if (userGuess < randomNumber)
+             {
+                 Console.WriteLine("Gjett litt højere");
+             }
+             else if (userGuess > randomNumber)
+             {
+                 Console.WriteLine("Tallet er litt lavere");
+             }
+
+         }
+     }
+     
+ 
+        
+        
+        
+        
+        
+        
+      
+    
+
+
+
+
+
